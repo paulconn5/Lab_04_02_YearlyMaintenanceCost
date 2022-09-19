0358260c9c2d095e23d9008c84bcda477dceb73f
Paul Connolly <paulconn5@gmail.com>
2022-09-18 21:47:12 -0400

    Finished task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double season1Cost = 10;
+        double season2Cost = 15;
+        double season3Cost = 25;
+        double season4Cost = 32.3;
+        double totalCost = season1Cost+season2Cost+season3Cost+season4Cost;
+
+        System.out.println("Season 1 cost: $" + season1Cost + ", Season 2 cost: $" + season2Cost + ", Season 3 cost: $" + season3Cost + ", Season 4 cost: $" + season1Cost +", Total: $" + totalCost );
     }
 }
